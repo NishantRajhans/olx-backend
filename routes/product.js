d3fe6301a0130a87c41cae053a50639482ef1818
NishantRajhans <nishantrajhans35@gmail.com>
2023-09-25 21:03:48 +0530

    update

--- routes/product.js
+++ routes/product.js
@@ -10,8 +10,6 @@ const {
 const { auth} = require("../middleware/auth")
 router.post("/createProduct",auth,createProduct)
 router.get("/getAllProduct", auth,getAllProduct)
-// Get Details for a Specific Courses
 router.get("/getFullProductDetais/:id", auth, getFullProductDetails)
-// Delete a Course
 router.delete("/deleteProduct", deleteProduct)
 module.exports = router
